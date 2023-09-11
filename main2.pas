00932a3099125a8f52fd31064433cbf08587c942
Anirudh Kaushal <anirudhkushal30@gmail.com>
2023-09-11 16:29:50 -0400

    added math operators program

---
+++ main2.pas
@@ -0,0 +1,18 @@
+// math operators 
+
+program MathOperatorsProgram;
+
+var
+    x: integer;
+    y: integer;
+
+begin
+    x := 9;
+    y := 4;
+    writeln(x / y);
+    writeln(x div y);
+    writeln(x * y);
+    writeln(x + y);
+    writeln(x - y);
+    writeln(x mod y);
+end.
